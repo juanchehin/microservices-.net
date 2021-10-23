a2f1d6f572a8fedd3d5ac2fd36e517cefec481b3
chehin <chehinjuan@gmail.com>
2021-10-23 10:29:16 -0300

    Creacion de las capas Infrastructure,Application y Domain de Clean Architecture

---
+++ src/Services/Ordening/Ordening.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ordening.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Services/Ordening/Ordening.infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ordening.infrastructure
+{
+    public class Class1
+    {
+    }
+}
