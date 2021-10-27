ae8b0f69d5450639e62e192bb653f02a933c402d
chehin <chehinjuan@gmail.com>
2021-10-27 20:56:48 -0300

    Checkout

--- src/Services/Ordening/Ordening.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordening/Ordening.Application/Mappings/MappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Ordening.Application.Features.Orders.CheckoutOrder;
 using Ordening.Application.Features.Queries.GetOrdersList;
 // using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 // using Ordering.Application.Features.Orders.Commands.UpdateOrder;
@@ -12,7 +13,7 @@ namespace Ordering.Application.Mappings
         public MappingProfile()
         {
             CreateMap<Order, OrdersVm>().ReverseMap();
-            // CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
             // CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
     }
